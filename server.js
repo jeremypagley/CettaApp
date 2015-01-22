13f70f92be934b9eaf6c07485ed95cca59f626ce
jeremypagley <jkpagley@yahoo.com>
2015-01-22 15:49:51 -0500

    Style Update, Game Section Code, Parallax Effect

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.set('views', __dirname +'/views');
+var path = require('path');
+
+app.set('views', path.join( __dirname +'/views'));
 app.set('view engine', 'ejs');
 
 app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
     res.render('index.html');
@@ -17,3 +20,5 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
    console.log('App listening on port 3000...');
 });
+
+module.exports = app;
